db4812ab1f9ade9d3fdecebb101d74380447f0cb
Ahamed1846 <116168645+Ahamed1846@users.noreply.github.com>
2023-01-23 18:01:41 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,34 @@
+class Blog {
+  // Progression 1: Add 2 methods - addTitle(), and addDescription()
+  constuctor(title, detail) {
+    this.title = title;
+    this.detail = detail;
+  }
+  addTitle() {
+    var title_card = document.createElement('h1');
+    title_card.setAttribute('id', 'blog-title');
+    document.getElementById('card-text').appendChild(title_card);
+    title_card.innerHTML += this.title;
+  }
+  addDescription() {
+    var description = document.createElement('p');
+    description.setAttribute('id', 'blog-description');
+    document.getElementById('card-text').appendChild(description);
+    description.innerHTML += this.detail;
+  }
+}
+
+// Progression 2: Setup an event listner - add two functions
+// 1. `helperAddPost()`
+// 2. `helperPost()`
+function helperAddPost() {
+  let popup = document.getElementById('popupContact');
+  popup.style.display = 'block';
+}
+function helperPost() {
+  let title = document.getElementById('title');
+  let detail = document.getElementById('detail');
+  const myBlog = new Blog(title.value, detail.value);
+  let popup = document.getElementById('popupContact');
+  popup.style.display = 'none';
+}
